05249287bccca853f6cf040417806991264268ec
Howard Chen <pohaoche@usc.edu>
2020-01-27 13:01:11 -0800

    add the sort icon

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -14,14 +14,30 @@ class TableHeader extends Component {
       sortColumn.order = "asc";
     }
     this.props.onSort(sortColumn);
-  }
+  };
+
+  renderSortIcon = column => {
+    const { sortColumn } = this.props;
+    if (column.path !== sortColumn.path) return null;
+    return sortColumn.order === "asc" ? (
+      <i className="fa fa-sort-asc"></i>
+    ) : (
+      <i className="fa fa-sort-desc"></i>
+    );
+  };
 
   render() {
     return (
       <thead>
         <tr>
           {this.props.columns.map(column => (
-            <th key={column.path || column.key} onClick={() => this.raiseSort(column.path)}>{column.label}</th>
+            <th
+              key={column.path || column.key}
+              onClick={() => this.raiseSort(column.path)}
+              className="clickable"
+            >
+              {column.label} {this.renderSortIcon(column)}
+            </th>
           ))}
         </tr>
       </thead>
