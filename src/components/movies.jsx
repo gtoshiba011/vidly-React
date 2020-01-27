593aacafa67088776a49c7a5f808286deeed57c1
Howard Chen <pohaoche@usc.edu>
2020-01-27 11:52:33 -0800

    sort the column in moviesTable

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -72,6 +72,7 @@ class Movies extends Component {
             onLike={this.handleLike}
             onDelete={this.handleDelete}
             onSort={this.handleSort}
+            sortColumn={sortColumn}
           />
           <Pagination
             itemsCount={filteredMovies.length}
@@ -105,14 +106,7 @@ class Movies extends Component {
     this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
-  handleSort = path => {
-    const sortColumn = { ...this.state.sortColumn };
-    if (sortColumn.path === path) {
-      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-    } else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
+  handleSort = sortColumn => {
     this.setState({ sortColumn });
   };
 }
