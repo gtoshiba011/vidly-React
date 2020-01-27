f45d7d0197a3ce98a088a46224db6823283bb05b
Howard Chen <pohaoche@usc.edu>
2020-01-27 12:13:28 -0800

    extract tableHeader from moviesTable

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,24 +1,27 @@
 import React, { Component } from "react";
 import Like from "./common/like";
-import TableHeader from './common/tableHeader';
+import TableHeader from "./common/tableHeader";
 
 class MoviesTable extends Component {
-
   columns = [
-    { path: 'title', label: 'Title' },
-    { path: 'genre.name', label: 'Genre' },
-    { path: 'numberInStock', label: 'Stock' },
-    { path: 'dailyRentalRate', label: 'Rate' },
-    { path: 'like' },
-    { path: 'delete' }
-  ]
+    { path: "title", label: "Title" },
+    { path: "genre.name", label: "Genre" },
+    { path: "numberInStock", label: "Stock" },
+    { path: "dailyRentalRate", label: "Rate" },
+    { path: "like" },
+    { path: "delete" }
+  ];
 
   render() {
     const { movies, onLike, onDelete, onSort, sortColumn } = this.props;
 
     return (
       <table className="table">
-        <TableHeader columns={this.columns} sortColumn={sortColumn} onSort={onSort} />
+        <TableHeader
+          columns={this.columns}
+          sortColumn={sortColumn}
+          onSort={onSort}
+        />
         <tbody>
           {movies.map(movie => (
             <tr key={movie._id}>
