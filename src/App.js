fdfa7a38cd6abd994760a03eb193545c673ba393
Howard Chen <pohaoche@usc.edu>
2020-01-28 16:56:44 -0800

    1. add NavBar 2. create components: Customers, Rentals, NotFound 3. route the URL to components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 import "./App.css";
+import NavBar from "./components/common/navBar";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 
 function App() {
   return (
-    // <main className='container'><h1>Hello World</h1></main>
     <main className="container">
-      <Movies />
+      <NavBar />
+      <div className="content">
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect exact from="/" to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </div>
     </main>
   );
 }
