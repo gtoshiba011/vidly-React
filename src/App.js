c3f0efac786fa04d49d56c7febed056be273874d
Howard Chen <pohaoche@usc.edu>
2020-01-30 00:52:19 -0800

    add RegisterForm

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import MovieDetails from "./components/movieDetails";
-import LoginForm from './components/loginForm';
+import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/customers" component={Customers} />
           <Route path="/rentals" component={Rentals} />
           <Route path="/login" component={LoginForm} />
+          <Route path="/register" component={RegisterForm} />
           <Route path="/not-found" component={NotFound} />
           <Redirect exact from="/" to="/movies" />
           <Redirect to="/not-found" />
