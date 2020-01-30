2cda619afdc56dd9533868191c96284bc1d54c3c
Howard Chen <pohaoche@usc.edu>
2020-01-29 23:48:55 -0800

    use ...rest

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-const Input = ({ name, label, error, value, onChange, type }) => {
+// const Input = ({ name, label, error, value, onChange, type }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
@@ -8,9 +9,10 @@ const Input = ({ name, label, error, value, onChange, type }) => {
         name={name}
         id={name}
         className="form-control"
-        value={value}
-        onChange={onChange}
-        type={type}
+        // value={value}
+        // onChange={onChange}
+        // type={type}
+        {...rest}
         // ref={this.username}
       />
       {error && <div className="alert alert-danger">{error}</div>}
