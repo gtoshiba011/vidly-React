e7a5c23ed522320becacb0e074ff3d5f716c78ef
Howard Chen <pohaoche@usc.edu>
2020-01-29 23:28:25 -0800

    extract reusable Form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import Joi from "joi-browser";
+import Form from "./common/form";
 import Input from "./common/input";
 
-class LoginForm extends Component {
+class LoginForm extends Form {
   // use Ref to bind input and state
   // username = React.createRef();
 
   state = {
-    account: { username: "", password: "" },
+    data: { username: "", password: "" },
     errors: {}
   };
 
@@ -25,72 +26,12 @@ class LoginForm extends Component {
   //   this.username.current.focus();
   // }
 
-  validate = () => {
-    const options = { abortEarly: false };
-    const { error } = Joi.validate(this.state.account, this.schema, options);
-
-    if (!error) return null;
-
-    const errors = {};
-    for (let item of error.details) {
-      errors[item.path[0]] = item.message;
-    }
-    return errors;
-
-    // const { account } = this.state;
-    // if (account.username.trim() === "") {
-    //   errors.username = "Username is required.";
-    // }
-    // if (account.password.trim() === "") {
-    //   errors.password = "Password is required.";
-    // }
-    // return Object.keys(errors).length === 0 ? null : errors;
-  };
-
-  validateProperty = ({ name, value }) => {
-    const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] };
-    const { error } = Joi.validate(obj, schema);
-
-    return error ? error.details[0].message : null;
-
-    // if (name === "username" && value.trim() === "")
-    //   return "Username is required.";
-    // if (name === "password" && value.trim() === "")
-    //   return "Password is required.";
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-
-    // do not use document in React
-    // const username = document.getElementById('username').value;
-
-    // use reference in React
-    // const username = this.username.current.value;
-
-    const errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-
-    // call the server
+  doSubmit = () => {
     console.log("Submitted");
   };
 
-  handleChange = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProperty(input);
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-
-    const account = { ...this.state.account };
-    // update accout dynamically -> use bracket notation
-    account[input.name] = input.value;
-    this.setState({ account, errors });
-  };
-
   render() {
-    const { account, errors } = this.state;
+    const { data, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -99,14 +40,14 @@ class LoginForm extends Component {
           <Input
             name="username"
             label="Username"
-            value={account.username}
+            value={data.username}
             onChange={this.handleChange}
             error={errors.username}
           />
           <Input
             name="password"
             label="Password"
-            value={account.password}
+            value={data.password}
             onChange={this.handleChange}
             error={errors.password}
           />
