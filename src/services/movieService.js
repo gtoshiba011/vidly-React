9bdde73a6cb5d050a2bdeffe1f81c4a69eb40c93
Howard Chen <pohaoche@usc.edu>
2020-02-01 23:11:21 -0800

    update config file

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,12 +1,12 @@
 import http from "./httpService";
-import config from "../config.json";
+import { apiUrl } from "../config.json";
 
 function getMovies() {
-  return http.get(config.apiEndpoint + "/movies");
+  return http.get(apiUrl + "/movies");
 }
 
 function deleteMovie(id) {
-  return http.delete(config.apiEndpoint + "/movies/" + id);
+  return http.delete(apiUrl + "/movies/" + id);
 }
 
 export { getMovies, deleteMovie };
