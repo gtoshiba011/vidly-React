a2108914c07e5c6d25244444e7287696cfdb1ef2
Howard Chen <pohaoche@usc.edu>
2020-02-03 14:40:59 -0800

    remove bi-directional dependency between httpService and authService -> auth depends on http

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 import logger from "./logService";
-import auth from "./authService";
-
-axios.defaults.headers.common["x-auth-token"] = auth.getJwt();
 
 axios.interceptors.response.use(null, error => {
   console.log("INTERCEPTORS CALLED");
@@ -20,9 +17,14 @@ axios.interceptors.response.use(null, error => {
   return Promise.reject(error);
 });
 
+function setJwt(jwt) {
+  axios.defaults.headers.common["x-auth-token"] = jwt;
+}
+
 export default {
   get: axios.get,
   post: axios.post,
   delete: axios.delete,
-  put: axios.put
+  put: axios.put,
+  setJwt
 };
